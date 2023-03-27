e1a23e8bc0158722f65a86477eb8d1368bb267a5
Utkarsh Mishra <79375111+UtkarshTheWise@users.noreply.github.com>
2023-03-27 13:52:06 +0530

    I lay google off

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
+// Show loader on page load
+window.onload = function() {
+    document.querySelector('.loader').style.display = 'flex';
+};
+  
+window.onload = function() {
+    setTimeout(function() {
+      var loader = document.getElementById("loader");
+      if (loader) {
+        loader.style.opacity = 0;
+        setTimeout(function() {
+          loader.parentNode.removeChild(loader);
+        }, 1000); // 1 second delay
+      }
+    }, 1000); // 2 seconds delay
+  }
+
 // disappearing navbar
-jQuery(document).on("scroll", function () {
-    if ($(document).scrollTop() > 200) {
-        $(".navb").css("background-color", "rgba(0,0,0,0.6)");
-        $(".disappear").css("top", "0");
+jQuery("#wrapper").on("scroll", function () {
+    if ($("#wrapper").scrollTop() > 200) {
+        $(".navb").css("background-color","rgba(0,0,0,0.6)");
+        $(".disappear").css("top","0");
     } else {
-        $(".navb").css("background", "none");
-        $(".disappear").css("top", "-200");
+        $(".navb").css("background","none");
+        $(".disappear").css("top","-200");
     }
 });
 
-// SIDENAV 
-function openNav() {
-    document.getElementById("sidenav").style.width = "100%";
-}
+$("#wrapper").on("scroll", function () {
+    if ($("#wrapper").scrollTop() > 200) {
+      $(".navb").addClass("navb-scroll");
+    } else {
+      $(".navb").removeClass("navb-scroll");
+    }
+  });
 
-function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-}
+  // SIDENAV 
+  function openNav(){
+      document.getElementById("sidenav").style.width="100%";
+  }
+  function closeNav () {
+      document.getElementById("sidenav").style.width="0";
+  }
 
 // utkarsh itna cool haina 
 
