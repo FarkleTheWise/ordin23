54753c6c217eb9e89efa3d08ef0ec371dc3ccd9e
Utkarsh Mishra <79375111+UtkarshTheWise@users.noreply.github.com>
2023-03-27 15:19:41 +0530

    Add files via upload

--- events/events.js
+++ events/events.js
@@ -1,3 +1,21 @@
+// Show loader on page load
+window.onload = function() {
+  document.querySelector('.loader').style.display = 'flex';
+};
+
+window.onload = function() {
+  setTimeout(function() {
+    var loader = document.getElementById("loader");
+    if (loader) {
+      loader.style.opacity = 0;
+      setTimeout(function() {
+        loader.parentNode.removeChild(loader);
+      }, 1000); // 1 second delay
+    }
+  }, 1000); // 2 seconds delay
+}
+
+
 $('.slide-nav').on('click', function(e) {
   e.preventDefault();
   // get current slide
